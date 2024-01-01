a628ad596f98c26d110ff53df3539186fef8b196
Minister <mundestephane13@gmail.com>
2024-01-01 15:03:26 +0100

    Made action buttons respnosive

--- src/components/cards/actions.jsx
+++ src/components/cards/actions.jsx
@@ -6,24 +6,22 @@ import {
   AiOutlineMessage,
 } from "react-icons/ai";
 
-export default function CardActions({id=5}) {
+export default function CardActions({ id = 5 }) {
   return (
-    <div className="grid grid-cols-3 w-full mt-8 p-3 justify-around items-center gap-3">
-      <button
-        className="bg-base-200 bg-opacity-30 disabled flex justify-around items-center p-2"
-      >
+    <div className="ml-auto grid grid-cols-3 max-tablet:w-full tablet:min-w-[600px] mt-8 p-3 justify-around items-center gap-3 text-white">
+      <button className="bg-green-300 disabled flex justify-around items-center p-2 rounded-xl">
         <AiOutlinePhone size={22} />
         <h4 className="font-bold text-xs">Audio Call</h4>
       </button>
-      <button
-        className="bg-base-200 bg-opacity-30 disabled flex justify-around items-center p-2 "
-      >
+      <button className="bg-blue-300 disabled flex justify-around items-center p-2 rounded-xl">
         <AiOutlineVideoCamera size={22} />
         <h4 className="font-bold text-xs">Video Call</h4>
       </button>
       <button className="bg-base-300  flex justify-around items-center p-2 rounded-xl">
         <AiOutlineMessage size={22} />
-        <h4 className="font-bold text-xs"><Link href={"/chat/"+id}>Message</Link></h4>
+        <h4 className="font-bold text-xs">
+          <Link href={"/chat/" + id}>Message</Link>
+        </h4>
       </button>
     </div>
   );
